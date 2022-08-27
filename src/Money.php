03c944aa9269b0bba230069c402804d3dc327690
bkhorvatSisterPC <bkhorvat.php@gmail.com>
2022-08-27 18:12:26 +0200

    Fix indenting, psr12

--- src/Money.php
+++ src/Money.php
@@ -7,123 +7,123 @@ use KrepyshSpec\World\Currency;
 
 class Money {
 
-  // Scale parameter for bc math functions
-  private const SCALE = 14;
+    // Scale parameter for bc math functions
+    private const SCALE = 14;
 
-  /**
-   * The variable $amount must be > 0.
-   */
+    /**
+     * The variable $amount must be > 0.
+     */
 
-  public function __construct(private string $amount, private string $currency)
-  {
-      $this->checkValueForPositiveNumber($amount);
+    public function __construct(private string $amount, private string $currency)
+    {
+        $this->checkValueForPositiveNumber($amount);
 
-      $currency = strtoupper(trim($currency));
-      $this->checkCurrencyForExistence($currency);
+        $currency = strtoupper(trim($currency));
+        $this->checkCurrencyForExistence($currency);
 
-      $this->currency = $currency;
-  }
+        $this->currency = $currency;
+    }
 
-  public function getAmount(): string
-  {
-      return $this->amount;
-  }
+    public function getAmount(): string
+    {
+        return $this->amount;
+    }
 
-  public function getCurrency(): string
-  {
-      return $this->currency;
-  }
+    public function getCurrency(): string
+    {
+        return $this->currency;
+    }
 
-  /**
-   * The amount in Money object must be positive. After addition, subtraction,
-   * multiplication, division, we get the object of money.
-   *
-   * Therefore, all $values must be positive.
-   *
-   * Adding, subtracting, multiplying by 0 does not make sense.
-   * Division by zero is not possible.
-   *
-   * $value must be a numeric string for calculation accuracy with bcmath.
-   */
-  private function checkValueForPositiveNumber(string $value): void
-  {
-      if(!is_numeric($value) || $value[0] === '0' || $value[0] === '-') {
-          throw new InvalidArgumentException('Value: ' . $value . 'is not positive number');
-      }
-  }
+    /**
+     * The amount in Money object must be positive. After addition, subtraction,
+     * multiplication, division, we get the object of money.
+     *
+     * Therefore, all $values must be positive.
+     *
+     * Adding, subtracting, multiplying by 0 does not make sense.
+     * Division by zero is not possible.
+     *
+     * $value must be a numeric string for calculation accuracy with bcmath.
+     */
+    private function checkValueForPositiveNumber(string $value): void
+    {
+        if(!is_numeric($value) || $value[0] === '0' || $value[0] === '-') {
+            throw new InvalidArgumentException('Value: ' . $value . 'is not positive number');
+        }
+    }
 
-  // The currency must be real, used by people.
-  private function checkCurrencyForExistence(string $currency): void
-  {
-      $allCurrencies = Currency::all();
+    // The currency must be real, used by people.
+    private function checkCurrencyForExistence(string $currency): void
+    {
+        $allCurrencies = Currency::all();
 
-      if(!in_array($currency, array_keys($allCurrencies), true)) {
-          throw new InvalidArgumentException('Invalid currency ' . $currency);
-      }
-  }
+        if(!in_array($currency, array_keys($allCurrencies), true)) {
+            throw new InvalidArgumentException('Invalid currency ' . $currency);
+        }
+    }
 
-  // When adding or subtracting, the currencies must be the same.
-  private function checkCurrencyForEquality(string $otherCurrency): void
-  {
-      if($this->currency !== $otherCurrency) {
-          throw new InvalidArgumentException('Currency ' . $otherCurrency . 'is not equals' . $this->currency);
-      }
-  }
+    // When adding or subtracting, the currencies must be the same.
+    private function checkCurrencyForEquality(string $otherCurrency): void
+    {
+        if($this->currency !== $otherCurrency) {
+            throw new InvalidArgumentException('Currency ' . $otherCurrency . 'is not equals' . $this->currency);
+        }
+    }
 
-  public function add(Money $addend): Money
-  {
-      $this->checkCurrencyForEquality($addend->getCurrency());
-      $sum = bcadd($this->amount, $addend->getAmount(), self::SCALE);
+    public function add(Money $addend): Money
+    {
+        $this->checkCurrencyForEquality($addend->getCurrency());
+        $sum = bcadd($this->amount, $addend->getAmount(), self::SCALE);
 
-      return new Money($sum, $this->currency);
-  }
+        return new Money($sum, $this->currency);
+    }
 
-  public function subtract(Money $subtrahend): Money|int
-  {
-      /**
-       * The subtrahend cannot be greater than the minuend, becouse
-       * the amount in Money object must be positive.
-       *
-       * If the subtrahend is equal to minuend return 0.
-       */
-      if(bccomp($subtrahend->getAmount(), $this->amount, self::SCALE) === 1) {
-          throw new InvalidArgumentException('Result can not be negative.');
-      }
+    public function subtract(Money $subtrahend): Money|int
+    {
+        /**
+         * The subtrahend cannot be greater than the minuend, becouse
+         * the amount in Money object must be positive.
+         *
+         * If the subtrahend is equal to minuend return 0.
+         */
+        if(bccomp($subtrahend->getAmount(), $this->amount, self::SCALE) === 1) {
+            throw new InvalidArgumentException('Result can not be negative.');
+        }
 
-      $this->checkCurrencyForEquality($subtrahend->getCurrency());
-      $result = bcsub($this->amount, $subtrahend->getAmount(), self::SCALE);
+        $this->checkCurrencyForEquality($subtrahend->getCurrency());
+        $result = bcsub($this->amount, $subtrahend->getAmount(), self::SCALE);
 
-      if(bccomp($subtrahend->getAmount(), $this->amount, self::SCALE) === 0) {
-          return 0;
-      }else{
-          return new Money($result, $this->currency);
-      }
-  }
+        if(bccomp($subtrahend->getAmount(), $this->amount, self::SCALE) === 0) {
+            return 0;
+        }else{
+            return new Money($result, $this->currency);
+        }
+    }
 
-  /**
-   * Multiplier and divisor must be a positive, numeric string.
-   *
-   * Multiplying and dividing currencies among themselves does not make sense.
-   * The exchange rate is a vector.
-   *
-   * You can multiply and divide a currency by a positive number.
-   *
-   */
-  public function multiply(string $multiplier): Money
-  {
-      $this->checkValueForPositiveNumber($multiplier);
+    /**
+     * Multiplier and divisor must be a positive, numeric string.
+     *
+     * Multiplying and dividing currencies among themselves does not make sense.
+     * The exchange rate is a vector.
+     *
+     * You can multiply and divide a currency by a positive number.
+     *
+     */
+    public function multiply(string $multiplier): Money
+    {
+        $this->checkValueForPositiveNumber($multiplier);
 
-      $result = bcmul($this->amount, $multiplier, self::SCALE);
+        $result = bcmul($this->amount, $multiplier, self::SCALE);
 
-      return new Money($result, $this->currency);
-  }
+        return new Money($result, $this->currency);
+    }
 
-  public function divide(string $divisor): Money
-  {
-      $this->checkValueForPositiveNumber($divisor);
+    public function divide(string $divisor): Money
+    {
+        $this->checkValueForPositiveNumber($divisor);
 
-      $result = bcdiv($this->amount, $divisor, self::SCALE);
+        $result = bcdiv($this->amount, $divisor, self::SCALE);
 
-      return new Money($result, $this->currency);
-  }
+        return new Money($result, $this->currency);
+    }
 }
--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -9,159 +9,159 @@ use InvalidArgumentException;
 class MoneyTest extends TestCase
 {
 
-  //  Test invalid amount in constructor
-  public function testInvalidAmountExceptionIsString(): void
-  {
-      $this->expectException(InvalidArgumentException::class);
-      new Money('asdk', 'USD');
-  }
+    //  Test invalid amount in constructor
+    public function testInvalidAmountExceptionIsString(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Money('asdk', 'USD');
+    }
 
-  public function testInvalidAmountExceptionStartWithiZero(): void
-  {
-      $this->expectException(InvalidArgumentException::class);
-      new Money('0123', 'USD');
-  }
+    public function testInvalidAmountExceptionStartWithiZero(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Money('0123', 'USD');
+    }
 
-  public function testInvalidAmountExceptionIsZero(): void
-  {
-      $this->expectException(InvalidArgumentException::class);
-      new Money('0', 'USD');
-  }
+    public function testInvalidAmountExceptionIsZero(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Money('0', 'USD');
+    }
 
-  public function testInvalidAmountExceptionBinary(): void
-  {
-      $this->expectException(InvalidArgumentException::class);
-      new Money('0b11111111', 'USD');
-  }
+    public function testInvalidAmountExceptionBinary(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Money('0b11111111', 'USD');
+    }
 
-  public function testInvalidAmountExceptionHexadecimal(): void
-  {
-      $this->expectException(InvalidArgumentException::class);
-      new Money('0x1A', 'USD');
-  }
+    public function testInvalidAmountExceptionHexadecimal(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Money('0x1A', 'USD');
+    }
 
-  public function testInvalidAmountExceptionOctal(): void
-  {
-      $this->expectException(InvalidArgumentException::class);
-      new Money('0o123', 'USD');
-  }
+    public function testInvalidAmountExceptionOctal(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Money('0o123', 'USD');
+    }
 
-  public function testInvalidAmountExceptionNegative(): void
-  {
-      $this->expectException(InvalidArgumentException::class);
-      new Money('-123', 'USD');
-  }
+    public function testInvalidAmountExceptionNegative(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Money('-123', 'USD');
+    }
 
-  //  Test format currency
-  public function testConstructorWithInvalidCurrency(): void
-  {
-      $money = new Money('1.05', 'usd');
-      $this->assertEquals('USD', $money->getCurrency());
+    //  Test format currency
+    public function testConstructorWithInvalidCurrency(): void
+    {
+        $money = new Money('1.05', 'usd');
+        $this->assertEquals('USD', $money->getCurrency());
 
-      $money = new Money('1.05', 'uSd');
-      $this->assertEquals('USD', $money->getCurrency());
+        $money = new Money('1.05', 'uSd');
+        $this->assertEquals('USD', $money->getCurrency());
 
-      $money = new Money('1.05', ' usd   ');
-      $this->assertEquals('USD', $money->getCurrency());
-  }
+        $money = new Money('1.05', ' usd   ');
+        $this->assertEquals('USD', $money->getCurrency());
+    }
 
-  //  Test getters
-  public function testMoneyGetters(): void
-  {
-      $money = new Money('1.05', 'USD');
-      $this->assertEquals('1.05', $money->getAmount());
-      $this->assertEquals('USD', $money->getCurrency());
-  }
+    //  Test getters
+    public function testMoneyGetters(): void
+    {
+        $money = new Money('1.05', 'USD');
+        $this->assertEquals('1.05', $money->getAmount());
+        $this->assertEquals('USD', $money->getCurrency());
+    }
 
-  //  Test equalsCurrency method
-  public function testEqualsCurrencyWithAdd(): void
-  {
-      $money1 = new Money('1.05', 'USD');
-      $money2 = new Money('2.2', 'EUR');
+    //  Test equalsCurrency method
+    public function testEqualsCurrencyWithAdd(): void
+    {
+        $money1 = new Money('1.05', 'USD');
+        $money2 = new Money('2.2', 'EUR');
 
-      $this->expectException(InvalidArgumentException::class);
-      $money1->add($money2);
-  }
+        $this->expectException(InvalidArgumentException::class);
+        $money1->add($money2);
+    }
 
-  //  Test CheckCurrencyForExistence method
-  public function testCheckCurrencyForExistence(): void
-  {
-      $this->expectException(InvalidArgumentException::class);
-      new Money('3.05', 'islam');
-  }
+    //  Test CheckCurrencyForExistence method
+    public function testCheckCurrencyForExistence(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Money('3.05', 'islam');
+    }
 
-  public function testEqualsCurrencyWithSubtract(): void
-  {
-      $money1 = new Money('3.05', 'USD');
-      $money2 = new Money('2.2', 'EUR');
+    public function testEqualsCurrencyWithSubtract(): void
+    {
+        $money1 = new Money('3.05', 'USD');
+        $money2 = new Money('2.2', 'EUR');
 
-      $this->expectException(InvalidArgumentException::class);
-      $money1->subtract($money2);
-  }
+        $this->expectException(InvalidArgumentException::class);
+        $money1->subtract($money2);
+    }
 
-  //  Test add
-  public function testAdd(): void
-  {
-      $money1 = new Money('1.05', 'USD');
-      $money2 = new Money('2.2', 'USD');
-      $money3 = $money1->add($money2);
-      $this->assertEquals('3.25000000000000', $money3->getAmount());
-      $this->assertEquals('USD', $money3->getCurrency());
-  }
+    //  Test add
+    public function testAdd(): void
+    {
+        $money1 = new Money('1.05', 'USD');
+        $money2 = new Money('2.2', 'USD');
+        $money3 = $money1->add($money2);
+        $this->assertEquals('3.25000000000000', $money3->getAmount());
+        $this->assertEquals('USD', $money3->getCurrency());
+    }
 
-  //  Test subtract
-  public function testSubtract(): void
-  {
-      $money1 = new Money('3.05', 'USD');
-      $money2 = new Money('2.02', 'USD');
-      $money3 = $money1->subtract($money2);
-      $this->assertEquals('1.03000000000000', $money3->getAmount());
-      $this->assertEquals('USD', $money3->getCurrency());
-  }
+    //  Test subtract
+    public function testSubtract(): void
+    {
+        $money1 = new Money('3.05', 'USD');
+        $money2 = new Money('2.02', 'USD');
+        $money3 = $money1->subtract($money2);
+        $this->assertEquals('1.03000000000000', $money3->getAmount());
+        $this->assertEquals('USD', $money3->getCurrency());
+    }
 
-  public function testSubtrahendIsEqualsMinuend(): void
-  {
-      $money1 = new Money('3', 'USD');
-      $money2 = new Money('3', 'USD');
-      $money3 = $money1->subtract($money2);
-      $this->assertEquals(0, $money3);
-  }
+    public function testSubtrahendIsEqualsMinuend(): void
+    {
+        $money1 = new Money('3', 'USD');
+        $money2 = new Money('3', 'USD');
+        $money3 = $money1->subtract($money2);
+        $this->assertEquals(0, $money3);
+    }
 
-  public function testSubtractIsNegative(): void
-  {
-      $money1 = new Money('2.02', 'USD');
-      $money2 = new Money('3.05', 'USD');
+    public function testSubtractIsNegative(): void
+    {
+        $money1 = new Money('2.02', 'USD');
+        $money2 = new Money('3.05', 'USD');
 
-      $this->expectException(InvalidArgumentException::class);
-      $money1->subtract($money2);
+        $this->expectException(InvalidArgumentException::class);
+        $money1->subtract($money2);
 
-  }
+    }
 
-  //  Test Multiply
-  public function testMultiply(): void
-  {
-      $money1 = new Money('10.02', 'USD');
-      $money2 = $money1->multiply('2');
+    //  Test Multiply
+    public function testMultiply(): void
+    {
+        $money1 = new Money('10.02', 'USD');
+        $money2 = $money1->multiply('2');
 
-      $this->assertEquals('20.04000000000000', $money2->getAmount());
-      $this->assertEquals('USD', $money2->getCurrency());
-  }
+        $this->assertEquals('20.04000000000000', $money2->getAmount());
+        $this->assertEquals('USD', $money2->getCurrency());
+    }
 
-  //  Test devide
-  public function testDivide(): void
-  {
-      $money1 = new Money('10.02', 'USD');
-      $money2 = $money1->divide('2');
+    //  Test devide
+    public function testDivide(): void
+    {
+        $money1 = new Money('10.02', 'USD');
+        $money2 = $money1->divide('2');
 
-      $this->assertEquals('5.01000000000000', $money2->getAmount());
-      $this->assertEquals('USD', $money2->getCurrency());
-  }
+        $this->assertEquals('5.01000000000000', $money2->getAmount());
+        $this->assertEquals('USD', $money2->getCurrency());
+    }
 
-  public function testDivideByZero(): void
-  {
-      $money1 = new Money('10.02', 'USD');
+    public function testDivideByZero(): void
+    {
+        $money1 = new Money('10.02', 'USD');
 
-      $this->expectException(InvalidArgumentException::class);
-      $money1->divide('0');
-  }
+        $this->expectException(InvalidArgumentException::class);
+        $money1->divide('0');
+    }
 }
